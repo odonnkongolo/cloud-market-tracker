a48a5d4c0655a78ca67840deda4279f3f6ed5eb0
Odon Nkongolo <odonnkongolo@gmail.com>
2026-06-11 14:47:37 +0100

    ops: upgrade node.js actions

--- infrastructure/eks.tf
+++ infrastructure/eks.tf
@@ -29,6 +29,11 @@ resource "aws_eks_cluster" "main" {
       aws_subnet.public_2.id
     ]
   }
+  # Add this block right here for new API access:
+  access_config {
+    authentication_mode                         = "API_AND_CONFIG_MAP"
+    bootstrap_cluster_creator_admin_permissions = true
+  }
   depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
 }
 
